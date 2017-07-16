d2d21af8f9a7cebe3dc22781052a1e434c4e2c9f
Bliss Man <blissman@gmail.com>
2017-07-16 19:15:06 -0400

    append styles using js

--- exchangeWidget.js
+++ exchangeWidget.js
@@ -50,7 +50,7 @@ XR.regexTest = function(element) {
 XR.getRate = function(element) {
     var currency = document.getElementById(element).value;
     var rate;
-    if(currency === XR.exchangeRates.base) {
+    if (currency === XR.exchangeRates.base) {
         rate = 1;
     } else {
         rate = XR.exchangeRates.rates[currency];
@@ -60,23 +60,51 @@ XR.getRate = function(element) {
 
 // update the converted value when fields change
 XR.updateConverted = function(element) {
-    var inputValue = parseFloat(document.getElementById(element + "-inputNumber").value);
-    var inputRate = XR.getRate(element + "-inputCurrency");
-    var outputRate = XR.getRate(element + "-outputCurrency");
-    var outputValue = Math.round((inputValue / inputRate * outputRate)*100)/100;
-    document.getElementById(element + "-outputNumber").value = outputValue;
+    if (!!XR.exchangeRates) {
+        if(document.getElementById(element + "-inputNumber").value === "") {
+            document.getElementById(element + "-outputNumber").value = null;
+        } else {
+            var inputValue = parseFloat(document.getElementById(element + "-inputNumber").value);
+            var inputRate = XR.getRate(element + "-inputCurrency");
+            var outputRate = XR.getRate(element + "-outputCurrency");
+            var outputValue = Math.round((inputValue / inputRate * outputRate) * 100) / 100;
+            document.getElementById(element + "-outputNumber").value = outputValue;
+        }
+    } else {
+        document.getElementById(element + "-outputNumber").value = "Server Error";
+    }
 }
 
 // populate the widget with html
 XR.populateWidget = function(element) {
     // need to figure out the widget html here
-    var content = "<h1> Currency Converter </h1>";
+    var content = "<div class=\"exchangeWidget\">";
+    content += "<h1> Currency Converter </h1>";
     content += "<h3> Type in amount and select currency: </h3>";
     content += "<input type=\"number\" id=\"" + element + "-inputNumber\" name=\"inputNumber\" oninput=\"XR.updateConverted(\'" + element + "\')\"><select id=\"" + element + "-inputCurrency\" onchange=\"XR.updateConverted(\'" + element + "\')\"><option value=\"CAD\">CAD</option><option value=\"USD\">USD</option><option value=\"EUR\">EUR</option></select>";
     content += "<h3> Converted amount: </h3>";
-    content += "<input type=\"number\" id=\"" + element + "-outputNumber\" name=\"outputNumber\" disabled><select id=\"" + element + "-outputCurrency\" onchange=\"XR.updateConverted(\'" + element + "\')\"><option value=\"CAD\">CAD</option><option value=\"USD\">USD</option><option value=\"EUR\">EUR</option></select>";
+    content += "<input type=\"text\" id=\"" + element + "-outputNumber\" name=\"outputNumber\" disabled><select id=\"" + element + "-outputCurrency\" onchange=\"XR.updateConverted(\'" + element + "\')\"><option value=\"CAD\">CAD</option><option value=\"USD\">USD</option><option value=\"EUR\">EUR</option></select>";
+    content += "<br><a href=\"#\">Disclaimer</a>"
+    content += "<div>"
 
     return content;
+}
+
+// this method appends style to the html
+XR.appendStyles = function() {
+    var styleNode = document.createElement("style");
+    // define the style
+    var style = ".exchangeWidget{";
+    style += "border-color: #000;";
+    style += "border-width: 1px;";
+    style += "}";
+    // create a style text node with the style defined above
+    var styleText = document.createTextNode(style);
+    styleNode.appendChild(styleText);
+    // get the head object
+    var head = document.getElementsByTagName("head")[0];
+    // append the style to the bottom of head
+    head.appendChild(styleNode);
 }
 
 // check the dom for elements with the exchangeWidget prefix
@@ -92,5 +120,6 @@ XR.init = function() {
 document.onreadystatechange = function() {
     if (document.readyState === "complete") {
         XR.init();
+        XR.appendStyles();
     }
 }
